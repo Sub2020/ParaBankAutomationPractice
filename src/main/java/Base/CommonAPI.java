3a1baa73a9b25d2d9af48f81a41e44cbcd043dd8
Subrat <logosubrat@gmail.com>
2022-07-22 19:00:47 -0400

    ParaBank Automation updated with report

--- src/main/java/Base/CommonAPI.java
+++ src/main/java/Base/CommonAPI.java
@@ -1,62 +1,177 @@
 package Base;
 
+
+import com.relevantcodes.extentreports.LogStatus;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+import reporting.ExtentManager;
+import reporting.ExtentTestManager;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.Date;
 
 public class CommonAPI {
 
+
     String currentDir = System.getProperty("user.dir");
+    String browserstackUsername = "subrat_GRk4Pm";
+    String browserstackPassword = "qVofjq3S4DoWizRLmPiZ";
+    String saucelabsUsername = "";
+    String saucelabsPassword = "";
+
+
     public WebDriver driver;
+    public static com.relevantcodes.extentreports.ExtentReports extent;
 
-    @Parameters({"os", "browser", "url"})
+    @BeforeSuite
+    public void extentSetup(ITestContext context) {
+        ExtentManager.setOutputDirectory(context);
+        extent = ExtentManager.getInstance();
+    }
+
     @BeforeMethod
-    public void setUp(String os, String browser, String url){
+    public void startExtent(Method method) {
+        String className = method.getDeclaringClass().getSimpleName();
+        String methodName = method.getName().toLowerCase();
+        ExtentTestManager.startTest(method.getName());
+        ExtentTestManager.getTest().assignCategory(new String[]{className});
+    }
+
+    protected String getStackTrace(Throwable t) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        t.printStackTrace(pw);
+        return sw.toString();
+    }
+
+    @AfterMethod
+    public void afterEachTestMethod(ITestResult result) {
+        ExtentTestManager.getTest().getTest().setStartedTime(this.getTime(result.getStartMillis()));
+        ExtentTestManager.getTest().getTest().setEndedTime(this.getTime(result.getEndMillis()));
+        String[] var2 = result.getMethod().getGroups();
+        int var3 = var2.length;
+
+        for(int var4 = 0; var4 < var3; ++var4) {
+            String group = var2[var4];
+            ExtentTestManager.getTest().assignCategory(new String[]{group});
+        }
+
+        if (result.getStatus() == 1) {
+            ExtentTestManager.getTest().log(LogStatus.PASS, "Test Passed");
+        } else if (result.getStatus() == 2) {
+            ExtentTestManager.getTest().log(LogStatus.FAIL, this.getStackTrace(result.getThrowable()));
+        } else if (result.getStatus() == 3) {
+            ExtentTestManager.getTest().log(LogStatus.SKIP, "Test Skipped");
+        }
+
+        ExtentTestManager.endTest();
+        extent.flush();
+
+
+
+        this.driver.quit();
+    }
+
+    @AfterSuite
+    public void generateReport() {
+        extent.close();
+    }
+
+    private Date getTime(long millis) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(millis);
+        return calendar.getTime();}
+
+
+    public void getCloudDriver(String envName, String username, String password, String os, String osVersion, String browser, String browserVersion) throws MalformedURLException {
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability("os", os);
+        cap.setCapability("os_version", osVersion);
+        cap.setCapability("browser", browser);
+        cap.setCapability("browser_version", browserVersion);
+        if (envName.equalsIgnoreCase("browserstack")) {
+            cap.setCapability("resolution", "1024x768");
+            this.driver = new RemoteWebDriver(new URL("http://" + username + ":" + password + "@hub-cloud.browserstack.com:80/wd/hub"), cap);
+        } else if (envName.equalsIgnoreCase("saucelabs")) {
+            this.driver = new RemoteWebDriver(new URL("http://" + username + ":" + password + "@ondemand.saucelabs.com:80/wd/hub"), cap);
+
+        }
+    }
+
+    public void getLocalDriver(String os, String browser) {
+
         if (os.equalsIgnoreCase("mac")) {
-            if (browser.equalsIgnoreCase("chrome")){
-                System.setProperty("webdriver.chrome.driver",currentDir + "\\driver\\mac\\chromedriver");
+            if (browser.equalsIgnoreCase("chrome")) {
+                System.setProperty("webdriver.chrome.driver", currentDir + "\\driver\\mac\\chromedriver");
                 driver = new ChromeDriver();
-            } else if (browser.equalsIgnoreCase("firefox")){
-                System.setProperty("webdriver.gecko.driver",currentDir +"\\driver\\mac\\geckodriver\\");
+            } else if (browser.equalsIgnoreCase("firefox")) {
+                System.setProperty("webdriver.gecko.driver", currentDir + "\\driver\\mac\\geckodriver\\");
                 driver = new FirefoxDriver();
             } else if (browser.equalsIgnoreCase("Edge")) {
-                System.setProperty("webdriver.edge.driver", currentDir +"\\driver\\mac\\msedgedriver");
+                System.setProperty("webdriver.edge.driver", currentDir + "\\driver\\mac\\msedgedriver");
                 driver = new EdgeDriver();
 
             }
 
-        } else if (os.equalsIgnoreCase("windows")){
-            if (browser.equalsIgnoreCase("chrome")){
-                System.setProperty("webdriver.chrome.driver",currentDir + "\\driver\\windows\\chromedriver.exe");
+        } else if (os.equalsIgnoreCase("windows")) {
+            if (browser.equalsIgnoreCase("chrome")) {
+                System.setProperty("webdriver.chrome.driver", currentDir + "\\driver\\windows\\chromedriver.exe");
                 driver = new ChromeDriver();
-            } else if (browser.equalsIgnoreCase("firefox")){
-                System.setProperty("webdriver.gecko.driver",currentDir + "\\driver\\windows\\geckodriver.exe");
+            } else if (browser.equalsIgnoreCase("firefox")) {
+                System.setProperty("webdriver.gecko.driver", currentDir + "\\driver\\windows\\geckodriver.exe");
                 driver = new FirefoxDriver();
             } else if (browser.equalsIgnoreCase("Edge")) {
-                System.setProperty("webdriver.edge.driver", currentDir + "\\driver\\windows\\msedgedriver.exe" );
+                System.setProperty("webdriver.edge.driver", currentDir + "\\driver\\windows\\msedgedriver.exe");
                 driver = new EdgeDriver();
-
             }
+        }
+    }
+
+
+    @Parameters({"useCloudEnv", "envName", "os", "osVersion", "browser", "browserVersion", "url"})
+    @BeforeMethod
+    public void setUp(@Optional("false") boolean useCloudEnv, @Optional("browserstack") String envName, @Optional("mac") String os, @Optional("Sierra") String osVersion, @Optional("chrome") String browser, @Optional("100") String browserVersion, @Optional("http://www.google.com") String url) throws MalformedURLException {
+        if (useCloudEnv) {
+            if (envName.equalsIgnoreCase("browserstack")) {
+                this.getCloudDriver(envName, this.browserstackUsername, this.browserstackPassword, os, osVersion, browser, browserVersion);
+            } else if (envName.equalsIgnoreCase("saucelabs")) {
+                this.getCloudDriver(envName, this.saucelabsUsername, this.saucelabsPassword, os, osVersion, browser, browserVersion);
+            }
+        } else {
+            this.getLocalDriver(os, browser);
         }
         driver.manage().window().maximize();
         driver.get(url);
 
+
+    }
+    public WebDriver getDriver() {
+        return this.driver;
     }
 
-    @AfterMethod
     public String getPageTitle() {
-        return driver.getTitle();
+        return this.driver.getTitle();
     }
 
-    @AfterMethod
-    public void tearDown() {
-        driver.close();
-    }
+
+
+//    @AfterMethod
+//    public void tearDown() {
+//        driver.close();
+//    }
 
 
     public void click(String locator) {
@@ -107,5 +222,3 @@ public class CommonAPI {
         System.out.println(path);
     }
 }
-
-
