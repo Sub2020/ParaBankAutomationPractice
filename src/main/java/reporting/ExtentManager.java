f865296c624f8a793deb3b02cc94aae323039b38
Subrat <logosubrat@gmail.com>
2022-07-22 19:04:28 -0400

    ParaBank Automation updated with report

---
+++ src/main/java/reporting/ExtentManager.java
@@ -0,0 +1,32 @@
+package reporting;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import org.testng.ITestContext;
+import org.testng.Reporter;
+
+import java.io.File;
+
+public class ExtentManager {
+    private static ExtentReports extent;
+    private static ITestContext context;
+
+    public ExtentManager() {
+    }
+
+    public static synchronized ExtentReports getInstance() {
+        if (extent == null) {
+            File outputDirectory = new File(context.getOutputDirectory());
+            File resultDirectory = new File(outputDirectory.getParentFile(), "html");
+            extent = new ExtentReports(System.getProperty("user.dir") + "/extent-report/extent-report.html", true);
+            Reporter.log("Extent Report Directory" + resultDirectory, true);
+            extent.addSystemInfo("Subrat Devkota", "Subrat Automation team").addSystemInfo("Environment", "QA").addSystemInfo("User Name", "Subrat Devkota");
+            extent.loadConfig(new File(System.getProperty("user.dir") + "/report-config.xml"));
+        }
+
+        return extent;
+    }
+
+    public static void setOutputDirectory(ITestContext context) {
+        ExtentManager.context = context;
+    }
+}
